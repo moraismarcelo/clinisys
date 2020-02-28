dfea318f8f0b089d2c3ea77b70fb28351baf3762
Marcelo Morais <morais.marcelo@outlook.com>
2020-02-28 20:50:54 -0300

    atualização patient controller e index

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -14,7 +14,9 @@ class PatientController extends Controller
      */
     public function index()
     {
-        return view('patient.index');
+        $patients = Patient::all();
+
+        return view('patient.index', compact('patients'));
     }
 
     /**
--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -6,4 +6,41 @@
     <a href="{{route('patient.create')}}">
         <button class="btn btn-primary">Adicionar</button>
     </a>
+
+    <br>
+    <br>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">CPF</th>
+            <th scope="col">NOME</th>
+            <th scope="col">TELEFONE</th>
+            <th scope="col">CONVÊNIO</th>
+            <th scope="col">AÇÂO</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          @foreach($patients as $patient)
+            <th scope="row">{{$patient->id}}</th>
+            <td>{{$patient->cpf}}</td>
+            <td>{{$patient->name}}</td>
+            <td>{{$patient->ddd1.$patient->cellphone1}}</td>
+            <td>{{$patient->health_insurance}}</td>
+            <td>
+                <button class="btn btn-sm btn-success ">Editar</button>
+                <button class="btn btn-sm btn-primary">Histórico</button>
+                <button class="btn btn-sm btn-danger">Excluir</button>
+
+
+
+
+            </td>
+
+        </tr>
+            @endforeach
+
+        </tbody>
+    </table>
 @endsection
